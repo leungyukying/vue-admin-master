71117150787ba3febe1bfd9e3685246f82643a8b
huangguochao <917929050@qq.com>
2019-04-16 17:22:54 +0800

    跑起程序

--- src/views/nav2/Page4.vue
+++ src/views/nav2/Page4.vue
@@ -11,23 +11,23 @@
   import { mapGetters } from 'vuex'
   import { mapActions } from 'vuex'
 
-  export default {
-    computed: {
-  	// 使用对象展开运算符将 getters 混入 computed 对象中
-    ...mapGetters([
-      'getCount'
-      // ...
-    ])
-    },
-    methods: {
-    ...mapActions([
-      'increment', // 映射 this.increment() 为 this.$store.dispatch('increment')
-      'decrement'
-    ])
-      //...mapActions({
-      //  add: 'increment' // 映射 this.add() 为 this.$store.dispatch('increment')
-      //})
-    }
-  }
+  // export default {
+  //   computed: {
+  // 	// 使用对象展开运算符将 getters 混入 computed 对象中
+  //   ...mapGetters([
+  //     'getCount'
+  //     // ...
+  //   ])
+  //   },
+  //   methods: {
+  //   ...mapActions([
+  //     'increment', // 映射 this.increment() 为 this.$store.dispatch('increment')
+  //     'decrement'
+  //   ])
+  //     //...mapActions({
+  //     //  add: 'increment' // 映射 this.add() 为 this.$store.dispatch('increment')
+  //     //})
+  //   }
+  // }
 
 </script>
