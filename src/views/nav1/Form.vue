5afeef38865f4444790473421d9877401447a498
huangguochao <917929050@qq.com>
2019-04-19 21:29:41 +0800

    修改页面样式

--- src/views/nav1/Form.vue
+++ src/views/nav1/Form.vue
@@ -1,14 +1,20 @@
 <template>
 	<div>
-		<el-form ref="form" :inline="true" :model="form" @submit.prevent="onSubmit" style="margin:20px;width:100%;min-width:600px;">
+		<el-form ref="form" label-position="right" label-width="70px" :model="form" @submit.prevent="onSubmit" style="margin:20px;width:95%;min-width:600px;">
 			<el-row :gutter="10">
 				<el-col :span="12">
 					<el-form-item label="HIS号">
-					<el-input v-model="form.hisNo" autosize="true"></el-input>
-				</el-form-item>
+						<el-input v-model="form.hisNo">
+							<el-select v-model="form.hisType" slot="prepend">
+								<el-option label="门诊" value="1"></el-option>
+								<el-option label="住院" value="2"></el-option>
+							</el-select>
+							<el-button class="el-icon-search" slot="append"></el-button>
+						</el-input>
+					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="姓名">
+					<el-form-item label="姓名" label-width="90px">
 						<el-input v-model="form.name"></el-input>
 					</el-form-item>
 				</el-col>
@@ -20,20 +26,22 @@
 						</el-select>
 					</el-form-item>	
 				</el-col>
-				<!-- <el-col :span="6">
-					<el-form-item label="年龄">
-						<el-input v-model="form.age"></el-input>
-					</el-form-item>
-				</el-col> -->
 			</el-row>
 			<el-row :gutter="10">
+				<el-col :span="6">
+					<el-form-item label="年龄">
+						<el-input v-model="form.age">
+							<template slot="append">岁</template>
+						</el-input>
+					</el-form-item>
+				</el-col>
 				<el-col :span="6">
 					<el-form-item label="联系电话">
 						<el-input v-model="form.phoneNum"></el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="门诊住院号">
+					<el-form-item label="门诊/住院号" label-width="90px">
 						<el-input v-model="form.outPatientNo"></el-input>
 					</el-form-item>
 				</el-col>
@@ -42,20 +50,20 @@
 						<el-input v-model="form.inPatientNo"></el-input>
 					</el-form-item>
 				</el-col>
+			</el-row>
+			<el-row :gutter="10">
 				<el-col :span="6">
 					<el-form-item label="病历号">
 						<el-input v-model="form.recordNum"></el-input>
 					</el-form-item>
 				</el-col>
-			</el-row>
-			<el-row :gutter="10">
 				<el-col :span="6">
 					<el-form-item label="身份证号">
 						<el-input v-model="form.cardNo"></el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="病人类型">
+					<el-form-item label="病人类型" label-width="90px">
 						<el-select v-model="form.patientType" placeholder="请选择病人类型">
 							<el-option label="男" value="M"></el-option>
 							<el-option label="女" value="F"></el-option>
@@ -67,9 +75,8 @@
 						<el-switch on-text="" off-text="" v-model="form.isRemain"></el-switch>
 					</el-form-item>
 				</el-col>
-				<el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
 			</el-row>
-			<el-row>
+			<el-row :gutter="10">
 				<el-col :span="6">
 					<el-form-item label="赔险设备">
 						<el-input v-model="form.equipment"></el-input>
@@ -81,10 +88,12 @@
 					</el-form-item>
 				</el-col>
 			</el-row>
-			<el-form-item>
-				<el-button type="primary">手工预约</el-button>
-				<el-button @click.native.prevent>重置</el-button>
-			</el-form-item>
+			<el-row>
+				<el-col :span="24" style="text-align:center">
+					<el-button type="primary">手工预约</el-button>
+					<el-button @click.native.prevent>重置</el-button>
+				</el-col>
+			</el-row>
 		</el-form>
 
 		<el-table :data="users" border style="width: 100%;">
@@ -122,12 +131,22 @@
 	</div>
 </template>
 
+<style>
+  .el-input .el-select .el-input {
+    width: 90px;
+  }
+  /* .el-input .el-button {
+
+  } */
+</style>
+
 <script>
 	export default {
 		data() {
 			return {
 				form: {
 					hisNo: '',
+					hisType: '1',
 					name: '',
 					sex: '',
 					age: '',
