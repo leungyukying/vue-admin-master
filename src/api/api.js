00b4e22e032a232c74a432cf63aa782aea0cab32
huangguochao <917929050@qq.com>
2019-04-20 22:04:26 +0800

    提交查询功能

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let base = '';
+let base = 'https://www.easy-mock.com/mock/5cbb1b5077d87e22c39d911f/graduate';
 
 export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
 
@@ -15,3 +15,5 @@ export const batchRemoveUser = params => { return axios.get(`${base}/user/batchr
 export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
 
 export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
+
+export const selectPatientInfo = params => { return axios.get(`${base}/patient/info`, { params: params }); };
--- src/views/nav1/Form.vue
+++ src/views/nav1/Form.vue
@@ -3,23 +3,23 @@
 		<el-form ref="form" label-position="right" label-width="70px" :model="form" @submit.prevent="onSubmit" style="margin:20px;width:95%;min-width:600px;">
 			<el-row :gutter="10">
 				<el-col :span="12">
-					<el-form-item label="HIS号">
+					<el-form-item label="HIS号" prop="hisNo">
 						<el-input v-model="form.hisNo">
 							<el-select v-model="form.hisType" slot="prepend">
 								<el-option label="门诊" value="1"></el-option>
 								<el-option label="住院" value="2"></el-option>
 							</el-select>
-							<el-button class="el-icon-search" slot="append"></el-button>
+							<el-button class="el-icon-search" slot="append" @click="search"></el-button>
 						</el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="姓名" label-width="90px">
+					<el-form-item label="姓名" label-width="90px" prop="name">
 						<el-input v-model="form.name"></el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="性别">
+					<el-form-item label="性别" prop="sex">
 						<el-select v-model="form.sex" placeholder="请选择性别">
 							<el-option label="男" value="M"></el-option>
 							<el-option label="女" value="F"></el-option>
@@ -29,41 +29,41 @@
 			</el-row>
 			<el-row :gutter="10">
 				<el-col :span="6">
-					<el-form-item label="年龄">
+					<el-form-item label="年龄" prop="age">
 						<el-input v-model="form.age">
 							<template slot="append">岁</template>
 						</el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="联系电话">
+					<el-form-item label="联系电话" prop="phoneNum">
 						<el-input v-model="form.phoneNum"></el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="门诊/住院号" label-width="90px">
+					<el-form-item label="门诊/住院号" label-width="90px" prop="outPatientNo">
 						<el-input v-model="form.outPatientNo"></el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="床号">
+					<el-form-item label="床号" prop="inPatientNo">
 						<el-input v-model="form.inPatientNo"></el-input>
 					</el-form-item>
 				</el-col>
 			</el-row>
 			<el-row :gutter="10">
 				<el-col :span="6">
-					<el-form-item label="病历号">
+					<el-form-item label="病历号" prop="recordNum">
 						<el-input v-model="form.recordNum"></el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="身份证号">
+					<el-form-item label="身份证号" prop="cardNo">
 						<el-input v-model="form.cardNo"></el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="6">
-					<el-form-item label="病人类型" label-width="90px">
+					<el-form-item label="病人类型" label-width="90px" prop="patientType">
 						<el-select v-model="form.patientType" placeholder="请选择病人类型">
 							<el-option label="男" value="M"></el-option>
 							<el-option label="女" value="F"></el-option>
@@ -78,12 +78,12 @@
 			</el-row>
 			<el-row :gutter="10">
 				<el-col :span="6">
-					<el-form-item label="赔险设备">
+					<el-form-item label="赔险设备" prop="equipment">
 						<el-input v-model="form.equipment"></el-input>
 					</el-form-item>
 				</el-col>
 				<el-col :span="18">
-					<el-form-item label="地址">
+					<el-form-item label="地址" prop="address">
 						<el-input v-model="form.address"></el-input>
 					</el-form-item>
 				</el-col>
@@ -91,7 +91,7 @@
 			<el-row>
 				<el-col :span="24" style="text-align:center">
 					<el-button type="primary">手工预约</el-button>
-					<el-button @click.native.prevent>重置</el-button>
+					<el-button @click="reset">重置</el-button>
 				</el-col>
 			</el-row>
 		</el-form>
@@ -135,12 +135,10 @@
   .el-input .el-select .el-input {
     width: 90px;
   }
-  /* .el-input .el-button {
-
-  } */
 </style>
 
 <script>
+	import { selectPatientInfo } from "@/api/api";
 	export default {
 		data() {
 			return {
@@ -160,12 +158,30 @@
 					equipment: '',
 					address: ''
 				},
-				users: []
+				users: [{
+					applyNo: "123",
+					hisProjectName: "测试",
+					checkItem: "头部"
+				}]
 			}
 		},
 		methods: {
 			onSubmit() {
 				console.log('submit!');
+			},
+			reset(){
+				this.$refs.form.resetFields();
+			},
+			search(){
+				debugger;
+				var patientParams = {
+					hisNo: this.form.hisNo,
+					hisType: this.form.hisType
+				};
+				selectPatientInfo(patientParams).then(data => {
+					this.form = data.data;
+					this.users = data.users;
+				});
 			}
 		}
 	}
