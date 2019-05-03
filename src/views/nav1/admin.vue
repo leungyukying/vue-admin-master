090f4c10e90a7cd8c3c6e14cdefb57d3ad92b9dc
huangguochao <917929050@qq.com>
2019-05-03 11:51:25 +0800

    页面框架搭建

---
+++ src/views/nav1/admin.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<el-menu class="el-menu-vertical-demo">
+
+		</el-menu>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+	
+
+</script>
+
+<style scoped>
+
+</style>
