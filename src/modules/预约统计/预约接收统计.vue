386517e96d93d7a046335879a36d9cce01d70bc6
LINZJAI <969060748@qq.com>
2019-05-27 02:30:25 +0800

    预约申请

--- src/modules/预约统计/预约接收统计.vue
+++ src/modules/预约统计/预约接收统计.vue
@@ -8,8 +8,8 @@
         <el-form :inline="true" :model="formInline" class="demo-form-inline">
           <el-form-item label="预约时间">
             <el-date-picker
-              v-model="value1"
-              type="datetimerange"
+              v-model="formInline.value1"
+              type="daterange"
               range-separator="至"
               start-placeholder="开始日期"
               end-placeholder="结束日期"
@@ -17,45 +17,63 @@
           </el-form-item>
 
           <el-form-item label="申请医院">
-            <el-select v-model="formInline.region" placeholder="申请医院">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+            <el-select v-model="formInline.region1">
+              <el-option v-for="(i,key) in yiyuan" :key="key" :label="i" :value="i"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="检查医院">
-            <el-select v-model="formInline.region" placeholder="检查医院">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+            <el-select v-model="formInline.region2">
+              <el-option v-for="(i,key) in yiyuan" :key="key" :label="i" :value="i"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="检查类型">
-            <el-select v-model="formInline.region" placeholder="检查类型">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+            <el-select v-model="formInline.region3">
+              <el-option v-for="(i,key) in leixin" :key="key" :label="i" :value="i"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="检查项目">
-            <el-select v-model="formInline.region" placeholder="检查项目">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+            <el-select v-model="formInline.region4">
+              <el-option v-for="(i,key) in xiangmu" :key="key" :label="i" :value="i"></el-option>
             </el-select>
           </el-form-item>
 
           <div class="btn-con">
-            <el-button type="primary">确认</el-button>
+            <el-button type="primary" @click="uploadsa">确认</el-button>
             <el-button type="primary">导出数据</el-button>
-            <el-button>重置</el-button>
+            <el-button @click="weqwq()">重置</el-button>
           </div>
         </el-form>
       </div>
 
       <div class="detail-con" style="width: calc(100vw - 240px)">
-        <el-radio-group v-model="showType" style="margin-bottom: 15px;">
+        <el-radio-group v-model="showType" @change="weqwewq" style="margin-bottom: 15px;">
           <el-radio-button label="表格"></el-radio-button>
           <el-radio-button label="图表"></el-radio-button>
         </el-radio-group>
-        <dataTable v-if="showType == '表格'"></dataTable>
-        <chart v-if="showType == '图表'"></chart>
+
+        <span v-if="showType == '表格'">
+          <el-table :data="tableData" border style="width: 100%">
+            <el-table-column prop="检查医院" label="检查医院" width="180"></el-table-column>
+            <el-table-column prop="预约检查人数" label="预约检查人数" width="180"></el-table-column>
+            <el-table-column prop="完成检查人数" label="完成检查人数"></el-table-column>
+            <el-table-column prop="预约检查量" label="预约检查量"></el-table-column>
+            <el-table-column prop="完成检查量" label="完成检查量"></el-table-column>
+            <el-table-column prop="完成检查量总费用" label="完成检查量总费用"></el-table-column>
+          </el-table>
+        </span>
+
+        <span v-if="showType == '图表'">
+          <div>
+            <v-chart :forceFit="true" :height="cxasda.height" :data="cxasda.data" :padding="[100]">
+              <v-coord type="rect" direction="LT"/>
+              <v-tooltip/>
+              <v-legend/>
+              <v-axis data-key="value" position="right"/>
+              <v-axis data-key="label" :label="cxasda.label"/>
+              <v-bar position="label*value" color="type" :adjust="cxasda.adjust"/>
+            </v-chart>
+          </div>
+        </span>
       </div>
     </div>
   </div>
@@ -63,24 +81,185 @@
 
 <script>
 import leftMenu from "./components/left-menu";
-import dataTable from "./components/dataTable.1";
-import chart from "./components/chart.1";
+const DataSet = require("@antv/data-set");
+import moment from "moment";
 export default {
   data() {
     return {
+      cxasda: {},
+      tableData: [
+        {
+          检查医院: "",
+          预约检查人数: "",
+          完成检查人数: "",
+          预约检查量: "",
+          完成检查量: "",
+          完成检查量总费用: ""
+        }
+      ],
+      yiyuan: ["A医院", "C医院", "E医院", "D医院", "F医院", "B医院"],
+      leixin: ["肠镜", "彩超", "胃镜"],
+      xiangmu: ["颈部平扫", "头部平扫", "胸部透视", "核磁共振"],
       formInline: {
         value1: [],
-        user: "",
-        region: ""
+        region1: "",
+        region2: "",
+        region3: "",
+        region4: ""
       },
       showType: "表格"
     };
   },
-  methods: {},
+  methods: {
+    weqwewq(val) {
+      console.log(val);
+      if (val == "图表") {
+        const dv = new DataSet.View().source(this.tableData);
+        dv.transform({
+          type: "fold",
+          fields: [
+            "预约检查人数",
+            "完成检查人数",
+            "预约检查量",
+            "完成检查量",
+            "完成检查量总费用"
+          ],
+          key: "type",
+          value: "value"
+        });
+        const data = dv.rows;
+
+        const label = { offset: 12 };
+        const adjust = [{ type: "dodge", marginRatio: 1 / 32 }];
+
+        this.cxasda = {
+          data,
+          height: 400,
+          label: label,
+          adjust: adjust
+        };
+      }
+    },
+    dateFormatToString(date) {
+      date = new Date(date);
+      var year = date.getFullYear();
+      var month = (date.getMonth() + 1).toString();
+      var day = date.getDate().toString();
+      if (month.length == 1) {
+        month = "0" + month;
+      }
+      if (day.length == 1) {
+        day = "0" + day;
+      }
+      var dateTime = year + "-" + month + "-" + day;
+      return dateTime;
+    },
+    async uploadsa() {
+      var msgBody = {
+        root: {
+          CountInfor: {
+            BeginDate: this.formInline.value1[0]
+              ? moment(this.formInline.value1[0]).format("YYYY-MM-DD")
+              : "",
+            EndDate: this.formInline.value1[1]
+              ? moment(this.formInline.value1[1]).format("YYYY-MM-DD")
+              : "",
+            StudyHospital: this.formInline.region2,
+            studyType: this.formInline.region3,
+            StudyItem: this.formInline.region4
+          }
+        }
+      };
+      var xdsrw = [];
+      var patientParams = {
+        msgHeader:
+          '{"root":{"HeaderInfor":{"serviceName":"AppInCount","sender":"' +
+          this.formInline.region1 +
+          '","event":"countPatientIn"}}}',
+        msgBody: JSON.stringify(msgBody)
+      };
+      var xsd = await this.$http.post(
+        "/AppCountInterface/AppCount.asmx/callInterface",
+        patientParams
+      );
+
+      xdsrw[0] = xsd.data[0].count;
+      var patientParams = {
+        msgHeader:
+          '{"root":{"HeaderInfor":{"serviceName":"AppInCount","sender":"' +
+          this.formInline.region1 +
+          '","event":"countCheckedIn"}}}',
+        msgBody: JSON.stringify(msgBody)
+      };
+      var xsd = await this.$http.post(
+        "/AppCountInterface/AppCount.asmx/callInterface",
+        patientParams
+      );
+
+      xdsrw[1] = xsd.data[0].count;
+      var patientParams = {
+        msgHeader:
+          '{"root":{"HeaderInfor":{"serviceName":"AppInCount","sender":"' +
+          this.formInline.region1 +
+          '","event":"countOrderIn"}}}',
+        msgBody: JSON.stringify(msgBody)
+      };
+      var xsd = await this.$http.post(
+        "/AppCountInterface/AppCount.asmx/callInterface",
+        patientParams
+      );
+
+      xdsrw[2] = xsd.data[0].count;
+      var patientParams = {
+        msgHeader:
+          '{"root":{"HeaderInfor":{"serviceName":"AppInCount","sender":"' +
+          this.formInline.region1 +
+          '","event":"countCheckedOrderIn"}}}',
+        msgBody: JSON.stringify(msgBody)
+      };
+      var xsd = await this.$http.post(
+        "/AppCountInterface/AppCount.asmx/callInterface",
+        patientParams
+      );
+
+      xdsrw[3] = xsd.data[0].count;
+      var patientParams = {
+        msgHeader:
+          '{"root":{"HeaderInfor":{"serviceName":"AppInCount","sender":"' +
+          this.formInline.region1 +
+          '","event":"countFeeIn"}}}',
+        msgBody: JSON.stringify(msgBody)
+      };
+      var xsd = await this.$http.post(
+        "/AppCountInterface/AppCount.asmx/callInterface",
+        patientParams
+      );
+      console.log(xsd);
+      xdsrw[4] = xsd.data == "" ? 0 : xsd.data[0].count;
+
+      this.tableData = [
+        {
+          检查医院: this.formInline.region2,
+          预约检查人数: xdsrw[0],
+          完成检查人数: xdsrw[1],
+          预约检查量: xdsrw[2],
+          完成检查量: xdsrw[3],
+          完成检查量总费用: xdsrw[4]
+        }
+      ];
+    },
+    weqwq() {
+      this.formInline = {
+        value1: [],
+        region1: "",
+        region2: "",
+        region3: "",
+        region4: ""
+      };
+    }
+  },
   components: {
-    leftMenu,
-    dataTable,
-    chart
+    leftMenu
   }
 };
 </script>
--- src/modules/预约统计/预约申请明细.vue
+++ src/modules/预约统计/预约申请明细.vue
@@ -8,8 +8,8 @@
         <el-form :inline="true" :model="formInline" class="demo-form-inline">
           <el-form-item label="预约时间">
             <el-date-picker
-              v-model="value1"
-              type="datetimerange"
+              v-model="formInline.value"
+              type="daterange"
               range-separator="至"
               start-placeholder="开始日期"
               end-placeholder="结束日期"
@@ -17,28 +17,30 @@
           </el-form-item>
 
           <el-form-item label="申请医院">
-            <el-select v-model="formInline.region" placeholder="申请医院">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+            <el-select v-model="formInline.region1">
+              <el-option v-for="(i,key) in yiyuan" :key="key" :label="i" :value="i"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="检查医院">
+            <el-select v-model="formInline.region2">
+              <el-option v-for="(i,key) in yiyuan" :key="key" :label="i" :value="i"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="检查类型">
-            <el-select v-model="formInline.region" placeholder="检查类型">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+            <el-select v-model="formInline.region3">
+              <el-option v-for="(i,key) in leixin" :key="key" :label="i" :value="i"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="检查项目">
-            <el-select v-model="formInline.region" placeholder="检查项目">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+          <!-- <el-form-item label="检查项目">
+            <el-select v-model="formInline.region4">
+              <el-option v-for="(i,key) in xiangmu" :key="key" :label="i" :value="i"></el-option>
             </el-select>
-          </el-form-item>
+          </el-form-item>-->
 
           <div class="btn-con">
-            <el-button type="primary">确认</el-button>
+            <el-button type="primary" @click="getData">确认</el-button>
             <el-button type="primary">导出数据</el-button>
-            <el-button>重置</el-button>
+            <el-button @click="weqwq">重置</el-button>
           </div>
         </el-form>
       </div>
@@ -48,7 +50,7 @@
           <el-radio-button label="表格"></el-radio-button>
           <el-radio-button label="图表"></el-radio-button>
         </el-radio-group>-->
-        <dataTable></dataTable>
+        <dataTable :tableData="tableData"></dataTable>
         <!-- <chart v-if="showType == '图表'"></chart> -->
       </div>
     </div>
@@ -59,18 +61,115 @@
 import leftMenu from "./components/left-menu";
 import dataTable from "./components/dataTable.2";
 import chart from "./components/chart.2";
+import { getList } from "../../api/api.4";
+import moment from "moment";
 export default {
   data() {
     return {
+      yiyuan: ["A医院", "C医院", "E医院", "D医院", "F医院", "B医院"],
+      leixin: ["肠镜", "彩超", "胃镜"],
+      xiangmu: ["颈部平扫", "头部平扫", "胸部透视", "核磁共振"],
       formInline: {
-        value1: [],
-        user: "",
-        region: ""
+        value: [],
+        region1: "",
+        region2: "",
+        region3: "",
+        region4: ""
       },
-      showType: "表格"
+      showType: "表格",
+      tableData: []
     };
   },
-  methods: {},
+  methods: {
+    getData() {
+      let h = {
+        "?xml": {
+          "@version": "1.0",
+          "@encoding": "utf-8"
+        },
+        root: {
+          HeaderInfor: {
+            sender: "发送方（请求的医院）",
+            serviceName: "searchAppInfor",
+            operatorId: sessionStorage.user
+              ? JSON.parse(sessionStorage.user).id
+              : "",
+            operator: sessionStorage.user
+              ? JSON.parse(sessionStorage.user).name
+              : "",
+            callOrg: "调用机构编码",
+            targetOrg: "目标机构编码",
+            event: "事件",
+            PostTime: "调用时间"
+          }
+        }
+      };
+
+      let b = {
+        "?xml": {
+          "@version": "1.0",
+          "@encoding": "utf-8"
+        },
+        root: {
+          searcahCondition: {
+            AppBeginTime: this.formInline.value[0]
+              ? moment(this.formInline.value[0]).format("YYYY-MM-DD")
+              : "",
+            AppEndTime: this.formInline.value[1]
+              ? moment(this.formInline.value[1]).format("YYYY-MM-DD")
+              : "",
+            AppStatus: null,
+            PatientName: null,
+            PhoneNumber: null,
+            PatientId: null,
+            APPHospital: this.formInline.region1,
+            AppDoctor: null,
+            ExcuteHospital: this.formInline.region2,
+            StudyType: this.formInline.region3
+          }
+        }
+      };
+      var patientParams = {
+        msgHeader: JSON.stringify(h),
+        msgBody: JSON.stringify(b)
+      };
+
+      this.$http
+        .post("/AppSearchInfor/searchInfor.asmx/callInterface ", patientParams)
+        .then(res => {
+          this.tableData = res.data.map(item => {
+            let type = ["", "门诊", "住院", "体检"];
+            return {
+              病人姓名: item.PatientName,
+              年龄: item.Age,
+              性别: item.Sex,
+              HIS号: item.HisCode,
+              病人来源: type[item.PatientType],
+              检查医院: item.ExcuteHospital,
+              检查类型: item.StudyType,
+              检查项目: item.StudyItem,
+              费用: item.ItemFee,
+              预约时间: item.AppTimeSeg,
+              申请医院: item.APPHospital,
+              申请医生: item.AppDoctor,
+              申请时间: item.AppTime
+            };
+          });
+        });
+    },
+    weqwq() {
+      this.formInline = {
+        value1: [],
+        region1: "",
+        region2: "",
+        region3: "",
+        region4: ""
+      };
+    }
+  },
+  created() {
+    this.getData();
+  },
   components: {
     leftMenu,
     dataTable,
